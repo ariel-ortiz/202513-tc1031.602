40ac2df55401ad267e6278c2dd387c69535d973f
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2025-08-27 22:24:27 +0000

    Add code to implement + operator for union

--- 01. DinoSet/main.cpp
+++ 01. DinoSet/main.cpp
@@ -4,18 +4,21 @@
 int main()
 {
     DinoSet a;
-    std::cout << a.to_string() << "\n";
+    std::cout << "a = " << a.to_string() << "\n";
 
     DinoSet b;
     b.add(DinoId::velociraptor);
     b.add(DinoId::tyrannosaurus);
-    std::cout << b.to_string() << "\n";
+    std::cout << "b = " << b.to_string() << "\n";
 
     DinoSet c;
     c.add(DinoId::tyrannosaurus);
     c.add(DinoId::procompsognathus);
     c.add(DinoId::pteranodon);
-    std::cout << c.to_string() << "\n";
+    std::cout << "c = " << c.to_string() << "\n";
+
+    DinoSet d = b + c;
+    std::cout << "d = " << d.to_string() << "\n";
 
     return 0;
 }
